7d6f36f81c7311ebadbe4b5b06f9dff868a061a6
vd <vdbazinga79@gmail.com>
2018-02-08 17:38:53 +0530

    python script

---
+++ gender_api.py
@@ -0,0 +1,15 @@
+import json
+import urllib2
+
+first_list = ["darshan","kiran","vijay","aniruddh","hemant","divy","divya","hemant","sabina"]
+
+myKey = "SVlpDsZvjtJXjjzxCH"
+names = ""
+data = []
+
+names = '; '.join(first_list)
+
+data = json.load(urllib2.urlopen("https://gender-api.com/get?key=" + myKey + "&name=" + names))
+
+for person in data["result"]:
+    print "name: " + person["name"] + " gender: " + person["gender"]
